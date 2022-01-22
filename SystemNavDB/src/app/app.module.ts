4375de33f7ca5d560307c33a629ff45848ccb735
Ethan Tsui <ettsui10@gmail.com>
2022-01-22 17:43:31 -0500

    added function login with use of firebase, set up for different menu roles for various staff

--- SystemNavDB/src/app/app.module.ts
+++ SystemNavDB/src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {environment} from "src/environments/environment";
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import {LoggedInGuard} from './logged-in.guard';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { MenuComponent } from './menu/menu.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ResourceSearchComponent } from './resource-search/resource-search.component';
+import { RegisterStaffComponent } from './register-staff/register-staff.component';
+import { VolunteerMenuComponent } from './volunteer-menu/volunteer-menu.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LogoutComponent,
+    MenuComponent,
+    ProfileComponent,
+    ResourceSearchComponent,
+    RegisterStaffComponent,
+    VolunteerMenuComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    FormsModule,
+    NgbModule
   ],
-  providers: [],
+  providers: [{ provide: LoggedInGuard, useClass: LoggedInGuard}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ SystemNavDB/src/app/volunteer-menu/volunteer-menu.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from '../authentication.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-volunteer-menu',
+  templateUrl: './volunteer-menu.component.html',
+  styleUrls: ['./volunteer-menu.component.css']
+})
+export class VolunteerMenuComponent implements OnInit {
+
+  constructor(private router: Router, private logoutService: AuthenticationService) { }
+
+  ngOnInit(): void {
+  }
+
+
+  async logout(): Promise<boolean> {
+    // if(this.logoutService.logout()){
+    //   this.router.navigate(['/']);
+    //   return true;
+    // }
+    return await this.logoutService.logout();
+
+  }
+
+}
