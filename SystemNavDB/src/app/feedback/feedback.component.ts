088254101d3ac48f9a7dd769333e3eff61b3bf0a
marlajazzar <mjazz069@uottawa.ca>
2022-04-10 18:45:05 -0400

    worte some comments

--- SystemNavDB/src/app/feedback/feedback.component.ts
+++ SystemNavDB/src/app/feedback/feedback.component.ts
@@ -16,11 +16,16 @@ export class FeedbackComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  //Displays a confirmation message once the user clicks on the submit button
   public showMyMessage=false
 
   showFeedbackMessage() {
+   
     setTimeout(() =>  {
+      //once the user clicks on the submit button, after 1 sec, the message will appear
       this.showMyMessage = true}, 1000)
+       //the message displays for 7 seconds and then disappears again
     setTimeout(() =>  {
       this.showMyMessage = false}, 7000)
     }
