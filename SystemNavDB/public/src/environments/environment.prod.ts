18b38e408e8f3251f9f607dbb724eea0fe253f59
Ethan Tsui <ettsui10@gmail.com>
2022-09-27 18:03:21 -0400

    successfully connected hosting

---
+++ SystemNavDB/public/src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyCBf6IWXDpR3xbQHkt0-HJE8TR6FBLP1cQ",
+    authDomain: "system-navigation-database.firebaseapp.com",
+    projectId: "system-navigation-database",
+    storageBucket: "system-navigation-database.appspot.com",
+    messagingSenderId: "462236230729",
+    appId: "1:462236230729:web:e2c9e20e6d121af70dcb98"
+  }};
